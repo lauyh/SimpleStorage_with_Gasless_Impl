6a20cc2ece4aa86e4a45bbe1b03b81ecbbfdc1ec
Richard <32272387+lauyh@users.noreply.github.com>
2022-05-22 00:58:32 +0800

    Update the test case

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,15 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
+const POLYGON_MUMBAI_TRUSTFORWARDER = "0x9399BB24DBB5C4b782C70c2969F58716Ebbd6a3b"
+
+describe("Simple storage", function () {
+  it("Should return value after adding the value", async function () {
     const contract = await ethers.getContractFactory("SimpleStorage");
-    const simpleStorage = await contract.deploy("Hello, world!");
+    const simpleStorage = await contract.deploy(POLYGON_MUMBAI_TRUSTFORWARDER);
     await simpleStorage.deployed();
+    await simpleStorage.setStorage("This is a test");
+    const tmp = await simpleStorage.getStorage()
+    expect(tmp).to.equal("This is a test");
   });
 });
